1bed5805573305dfefc9cfba38948ec8288aaa32
Carlm91 <callemickelson@hotmail.com>
2022-08-22 14:55:06 +0200

    Second commit!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!Change one");
+        System.out.println("Hello world!Change one + Change 2");
     }
 }
