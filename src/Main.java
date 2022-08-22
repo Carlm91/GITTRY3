2e5a363fd84e3045f92cfdbcd0866a7702cc9996
Carlm91 <callemickelson@hotmail.com>
2022-08-22 15:28:33 +0200

    new feature!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!Change one + Change 2+Change 3 + change to the clone");
-        System.out.println("master changed");
+        System.out.println("this is a new feature");
     }
 }
