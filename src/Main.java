b1f993ed445383a9ead5e4530805136f3ba531a6
Carlm91 <callemickelson@hotmail.com>
2022-08-22 15:19:31 +0200

    clone change 2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!Change one + Change 2+Change 3 + change to the clone");
+        System.out.println("change to clone ");
     }
 }
